83c14ca2716992f75f2a613bbd9b9c30ac0e714c
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-12-25 21:03:03 -0200

    Add utils Add AddEntry class

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>guilherme !!</Text>
+        <AddEntry/>
       </View>
     );
   }
