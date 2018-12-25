633ef8c3e1f145c899c0e95286330ed9e130fc29
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-12-25 21:35:53 -0200

    Add new components: -DateHeader -Slider -Steppers

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helper'
+import Slider from './Slider'
+import Steppers from './Steppers'
+import DateHeader from './DateHeader'
 
 export default class AddEntry extends Component {
     state = {
@@ -38,9 +41,34 @@ export default class AddEntry extends Component {
         }))
     }
     render() {
+        const metaInfo = getMetricMetaInfo();
+
         return (
             <View>
-                {getMetricMetaInfo('run').getIcon()}
+                <DateHeader date={(new Date()).toLocaleDateString()}/>
+                {Object.keys(metaInfo).map((key) => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+
+
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+                            {type === 'slider'
+                                ? <Slider
+                                    value={value}
+                                    onChange={(value) => this.slide(key, value)}
+                                    {...rest}
+                                />
+                                : <Steppers
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                />}
+                        </View>
+                    )
+                })}
             </View>
         )
     }
