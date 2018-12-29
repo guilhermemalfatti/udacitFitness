c4c43640757c2e65e94cc65a883616a2e3018ee8
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-12-29 08:05:49 -0200

    Logic to show a msg, if the info ws alredy logged

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,15 +4,18 @@ import { getMetricMetaInfo, timeToString } from '../utils/helper'
 import UdaciSlider from './UdaciSlider'
 import Steppers from './Steppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
-function SubmitBtn ({ onPress }) {
+function SubmitBtn({ onPress }) {
     return (
-      <TouchableOpacity
-        onPress={onPress}>
-          <Text>SUBMIT</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
     )
-  }
+}
+
 
 export default class AddEntry extends Component {
     state = {
@@ -53,22 +56,48 @@ export default class AddEntry extends Component {
         const key = timeToString()
         const entry = this.state
 
-         // Update Redux
+        // Update Redux
 
-         this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
-         // Navigate to home
+        // Navigate to home
 
-         // Save to "DB"
+        // Save to "DB"
 
-         // Clear local notification
-      }
+        // Clear local notification
+    }
+
+    reset = () => {
+        const key = timeToString()
+
+        // Update Redux
+
+        // Route to Home
+
+        // Update "DB"
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo();
 
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                        name={'md-happy'}
+                        size={100}
+                    />
+                    <Text>You already logged your information for today.</Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
+
         return (
             <View>
-                <DateHeader date={(new Date()).toLocaleDateString()}/>
+                <DateHeader date={(new Date()).toLocaleDateString()} />
                 <Text>{JSON.stringify(this.state)}</Text>
                 {Object.keys(metaInfo).map((key) => {
                     const { getIcon, type, ...rest } = metaInfo[key]
